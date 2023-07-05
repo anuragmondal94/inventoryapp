396fd8f9246eb2f941cbb8c6b05cbabb542e18b4
amondal15 <m.anurag@crispmails.com>
2023-07-04 20:25:38 -0500

    Life cycle hooks contd and containers

--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit, QueryList, ViewChild, ViewChildren } from '@angular/core';
 import { Room, RoomList } from './rooms';
+import { HeaderComponent } from '../header/header.component';
 
 @Component({
   selector: 'app-rooms',
   templateUrl: './rooms.component.html',
   styleUrls: ['./rooms.component.scss']
 })
-export class RoomsComponent {
+export class RoomsComponent implements OnInit, AfterViewInit {
+  ngAfterViewInit() {
+   this.headerComponent.title = "Rooms View"
+  //  this.headerChildrenComponent.last.title = "Last Title"
+  }
 
   hotelName = 'Hyatt';
   numberOfRooms = 10
@@ -24,8 +29,13 @@ export class RoomsComponent {
   title = 'Room List'
 
   roomList: RoomList[] = [];
- 
-  ngOnInit(): void{
+
+  @ViewChild(HeaderComponent ) headerComponent !: HeaderComponent
+
+  @ViewChildren(HeaderComponent) headerChildrenComponent !: QueryList<HeaderComponent>
+
+  ngOnInit(): void {
+    // console.log(this.headerComponent)
 
     this.roomList = [
       {
@@ -36,9 +46,9 @@ export class RoomsComponent {
         photos: 'https://unsplash.com/photos/YMOHw3F1Hdk?utm_source=unsplash&utm_medium=referral&utm_content=creditShareLink',
         checkInTIme: new Date('11-Nov-2023'),
         checkoutTime: new Date('12-Nov-2023'),
-        rating : 3.48
+        rating: 3.48
       },
-  
+
       {
         roomNumber: 2,
         roomType: 'Suite',
@@ -47,9 +57,9 @@ export class RoomsComponent {
         photos: 'https://unsplash.com/photos/YMOHw3F1Hdk?utm_source=unsplash&utm_medium=referral&utm_content=creditShareLink',
         checkInTIme: new Date('11-Nov-2023'),
         checkoutTime: new Date('12-Nov-2023'),
-        rating : 4.78
+        rating: 4.78
       },
-  
+
       {
         roomNumber: 3,
         roomType: 'Penthouse',
@@ -58,9 +68,9 @@ export class RoomsComponent {
         photos: 'https://unsplash.com/photos/YMOHw3F1Hdk?utm_source=unsplash&utm_medium=referral&utm_content=creditShareLink',
         checkInTIme: new Date('11-Nov-2023'),
         checkoutTime: new Date('12-Nov-2023'),
-        rating : 4.32
+        rating: 4.32
       }
-  
+
     ]
 
   }
@@ -72,22 +82,22 @@ export class RoomsComponent {
 
   }
 
-  selectRoom(room: RoomList){
+  selectRoom(room: RoomList) {
     this.selectedRoom = room
     this.title = "RoomS!!!"
 
   }
 
   addRoom() {
-    const room : RoomList = {
+    const room: RoomList = {
       roomNumber: 4,
-        roomType: 'Additional Room',
-        amenites: 'Air Conditioner , WIFI, Tv, Kitchen, Lounge, Study',
-        price: 2000,
-        photos: 'https://unsplash.com/photos/YMOHw3F1Hdk?utm_source=unsplash&utm_medium=referral&utm_content=creditShareLink',
-        checkInTIme: new Date('11-Nov-2023'),
-        checkoutTime: new Date('12-Nov-2023'),
-        rating : 3.99
+      roomType: 'Additional Room',
+      amenites: 'Air Conditioner , WIFI, Tv, Kitchen, Lounge, Study',
+      price: 2000,
+      photos: 'https://unsplash.com/photos/YMOHw3F1Hdk?utm_source=unsplash&utm_medium=referral&utm_content=creditShareLink',
+      checkInTIme: new Date('11-Nov-2023'),
+      checkoutTime: new Date('12-Nov-2023'),
+      rating: 3.99
     }
 
     // this.roomList.push(room)
